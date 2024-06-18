917c931bd4a3562ab87c8aade45f067bd69a229e
Boominathan <eboominathan@gmail.com>
2024-06-18 14:20:54 +0530

    models completed

--- lesson01/myproject/posts/views.py
+++ lesson01/myproject/posts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Posts
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'posts/post_list.html')
+    posts = Posts.objects.all().order_by('-date')
+    return render(request, 'posts/post_list.html',{'posts':posts})
+
+
