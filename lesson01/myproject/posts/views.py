1c55bd3c141277be137caba61ec3fa8d4d1a24cf
Boominathan <eboominathan@gmail.com>
2024-06-18 14:41:16 +0530

    url making completed

--- lesson01/myproject/posts/views.py
+++ lesson01/myproject/posts/views.py
@@ -1,9 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Posts
+from django.http import HttpResponse
 
 # Create your views here.
-def post_list(request):
+def posts_list(request):
     posts = Posts.objects.all().order_by('-date')
     return render(request, 'posts/post_list.html',{'posts':posts})
+
+def post_page(request, slug) -> HttpResponse:
+   post =  Posts.objects.get(slug=slug)    
+   return render(request, 'posts/post_page.html', {'post':post})
 
 
